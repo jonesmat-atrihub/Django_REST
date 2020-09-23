1c940a9866dd07ab91cc8f0af3e104c3c1af4152
Matthew Jones <jonesmat@atrihub.io>
2020-09-23 13:55:57 -0700

    feat (MJ REST): complete Part 5
    Completed Part 5 of the Django REST tutorial
    
    [ICTRAINING-471]

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -6,10 +6,12 @@ from .models import Snippet
 
 class SnippetSerializer( serializers.ModelSerializer ):
     owner = serializers.ReadOnlyField( source='owner.username' )
+    highlight = serializers.HyperlinkedIdentityField( view_name='snippet-highlight', format='html' )
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
 
 class UserSerializer( serializers.ModelSerializer ):
@@ -17,4 +19,4 @@ class UserSerializer( serializers.ModelSerializer ):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets',]
+        fields = ['url', 'id', 'username', 'snippets', ]
